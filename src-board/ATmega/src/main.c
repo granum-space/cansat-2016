8f43300099dcf941025514f96ca221806d2a0911
Kirs67 <kirilel200267@gmail.com>
2017-05-02 21:34:59 +0300

    Доведены до работы многие датчики

--- src-board/ATmega/src/main.c
+++ src-board/ATmega/src/main.c
@@ -16,8 +16,10 @@
 #include "rscs/adc.h"
 #include "rscs/adxl345.h"
 #include "rscs/bmp280.h"
+#include "rscs/dht22.h"
 #include "rscs/ds18b20.h"
 #include "rscs/sdcard.h"
+#include "rscs/timeservice.h"
 #include "rscs/uart.h"
 #include "rscs/stdext/stdio.h"
 
@@ -26,45 +28,62 @@
 rscs_ads1115_t * ads1115;
 rscs_adxl345_t * adxl345;
 rscs_bmp280_descriptor_t * bmp280;
+rscs_dht22_t * dht22;
 rscs_ds18b20_t * ds18b20;
 rscs_sdcard_t * sdcard;
 rscs_uart_bus_t * uart_gps;
 rscs_uart_bus_t * uart_data;
 
+gr_telemetry_t packet = {	.marker = 0xFCFC,
+							.number = 0,
+							.pressure = 0,
+							.temperature_bmp = 0,
+							.temperature_ds18 = 0,
+							.temperature_dht = 0,
+							.humidity = 0,
+							.luminosity1 = 0,
+							.luminosity2 = 0,
+							.luminosity3 = 0,
+							.temperature_soil1 = 0,
+							.temperature_soil2 = 0,
+							.temperature_soil3 = 0,
+							.latitude = 56.72666666666667,
+							.longtitude = 37.716944444444444,
+							.height = 143
+};
+
+int32_t rawpress, rawtemp;
+const rscs_bmp280_calibration_values_t * calvals;
+
+uint8_t dht_untilrequest = 0;
+
 static void init();
 static void sensupdate();
+inline static void sign_packet(gr_telemetry_t * packet);
 
 #define OPR(OP) error = OP; if(error != RSCS_E_NONE) printf("ERROR %d", error);
 rscs_e error = RSCS_E_NONE;
 
 int main() {
-	init();
-	int16_t ds18_data = 0;
+	packet.soilresist_data[0].adc = 0;
+	packet.soilresist_data[0].digipot = 60529;
+	packet.soilresist_data[1].adc = 0;
+	packet.soilresist_data[1].digipot = 64535;
+	packet.soilresist_data[2].adc = 0;
+	packet.soilresist_data[2].digipot = 65535;
 
-	int16_t x, y, z;
-	float x_g, y_g, z_g;
-	int32_t rawpress, rawtemp, pres, temp;
+	init();
 
 	while(1) {
-		while(1) if(rscs_ds18b20_check_ready()) break;
-		OPR( rscs_ds18b20_read_temperature(ds18b20, &ds18_data) )
-		OPR( rscs_ds18b20_start_conversion(ds18b20) )
-		RSCS_DEBUG("DS18B20: %f\n", ds18_data / 16.0f);
-
-		OPR( rscs_adxl345_GetGXYZ(adxl345, &x, &y, &z, &x_g, &y_g, &z_g) )
-		RSCS_DEBUG("ADXL345: %f  %f  %f   %d  %d  %d\n", x_g, y_g, z_g, x, y, z);
-
-		RSCS_DEBUG("BMP280: status: %x\n", rscs_bmp280_read_status(bmp280));
-		OPR( rscs_bmp280_read(bmp280, &rawpress, &rawtemp) )
-		rscs_bmp280_calculate(rscs_bmp280_get_calibration_values(bmp280), rawpress, rawtemp, &pres, &temp);
-		RSCS_DEBUG("BMP280: %ld  %ld   %ld  %ld\n", pres, temp, rawpress, rawtemp);
-		_delay_ms(100);
+		sensupdate();
 	}
 
 	return 0;
 }
 
 static void init() {
+
+	rscs_time_init(); //Служба времени
 
 	{ //UART для данных и дебага (ID_UART0)
 		uart_data = rscs_uart_init(RSCS_UART_ID_UART0, 	RSCS_UART_FLAG_ENABLE_RX
@@ -77,6 +96,7 @@ static void init() {
 		rscs_uart_set_stop_bits(uart_data, RSCS_UART_STOP_BITS_ONE);
 
 		RSCS_DEBUG_INIT(uart_data)
+		stdin = stdout = rscs_make_uart_stream(uart_data);
 	}
 
 	rscs_i2c_init();
@@ -96,17 +116,64 @@ static void init() {
 				.standbytyme = RSCS_BMP280_STANDBYTIME_62DOT5MS,
 				.filter = RSCS_BMP280_FILTER_X16
 		};
-		OPR( rscs_bmp280_set_config(bmp280, &params) )
+		OPR( rscs_bmp280_setup(bmp280, &params) )
 		OPR( rscs_bmp280_changemode(bmp280, RSCS_BMP280_MODE_NORMAL) )
+		calvals = rscs_bmp280_get_calibration_values(bmp280);
 	}
 
 	{ //DS18B20
 		ds18b20 = rscs_ds18b20_init(0x00); //не используем адресацию
-		rscs_ds18b20_start_conversion(ds18b20);
+		OPR( rscs_ds18b20_start_conversion(ds18b20) )
+	}
+
+	{ //DHT22
+		dht22 = rscs_dht22_init(&PORTB, &PINB, &DDRB, 4, 7.0f);
 	}
 
 }
 
 static void sensupdate() {
 
+	for(int i = 0; i < 10; i++) {
+		RSCS_DEBUG("ADXL345: reading: %d\n", rscs_adxl345_read(adxl345, &(packet.accelerations[i].x), &(packet.accelerations[i].y), &(packet.accelerations[i].z)));
+		_delay_ms(100);
+	}
+
+	printf("BMP: status: 0x%02X\n", rscs_bmp280_read_status(bmp280));
+	printf("BMP: status: %d\n", rscs_bmp280_read(bmp280, &rawpress, &rawtemp));
+	rscs_bmp280_calculate(rscs_bmp280_get_calibration_values(bmp280), rawpress, rawtemp, &(packet.pressure), &(packet.temperature_bmp));
+
+	printf("BMP: p %ld  t %ld   %ld %ld\n", rawpress, rawtemp, packet.pressure, packet.temperature_bmp);
+
+
+	if(dht_untilrequest == 0) {
+		rscs_dht22_read(dht22, &(packet.humidity), &(packet.temperature_dht));
+		dht_untilrequest  = 2;
+		printf("DHT: %d %d\n", packet.humidity, packet.temperature_dht);
+	}
+	else dht_untilrequest--;
+
+	if(rscs_ds18b20_check_ready()) {
+		RSCS_DEBUG("DS18B20: reading: %d\n", rscs_ds18b20_read_temperature(ds18b20, &(packet.temperature_ds18) ) );
+		RSCS_DEBUG("DS18B20: %d\n", packet.temperature_ds18);
+		RSCS_DEBUG("DS18B20: starting conversion: %d\n", rscs_ds18b20_start_conversion(ds18b20));
+	}
+
+
+	sign_packet(&packet);
+	rscs_uart_write(uart_data, &packet, sizeof(packet));
+	_delay_ms(100);
+}
+
+inline static void sign_packet(gr_telemetry_t * packet) {
+	packet->number++;
+	packet->time = 0;
+	packet->checksumm = 0;
+
+	for(int i = 0; i < ( sizeof(gr_telemetry_t) - sizeof(packet->checksumm) ); i++) {
+		packet->checksumm += ((uint8_t *) packet)[i];
+	}
+
+	packet->time = rscs_time_get();
+	packet->checksumm += packet->time;
 }
---
+++ src-board/common/comm_def.h
@@ -0,0 +1,39 @@
+typedef struct {
+	int16_t x, y, z;
+} accelerations_t;
+
+typedef struct {
+	uint16_t marker; //Must be 0x FIXME выбрать маркер пакета
+
+	uint16_t number;
+
+	int32_t pressure;
+	int32_t temperature_bmp;
+
+	int16_t temperature_ds18;
+
+	int16_t temperature_dht;
+	uint16_t humidity;
+
+	accelerations_t accelerations[10];
+
+	uint16_t luminosity1, luminosity2, luminosity3;
+
+	uint16_t temperature_soil1, temperature_soil2, temperature_soil3;
+
+	struct {
+		uint16_t adc;
+		uint16_t digipot;
+	} soilresist_data[3];
+
+	float latitude, longtitude, height;
+
+	uint32_t time;
+
+	uint32_t checksumm;
+} gr_telemetry_t;
+
+typedef struct {
+	uint16_t size;
+	accelerations_t * data;
+} gr_accelerationspack_t; //FIXME подумать над названием
