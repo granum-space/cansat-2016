b6d70becb7235befb007601fcaabdcf8d89ccad2
vasilly.prokopyev <vasilly.prokopyev@gmail.com>
2017-05-04 10:48:20 +0300

    Добавил настройки

--- src-board/ATmega/src/bosh_bmp280.c
+++ src-board/ATmega/src/bosh_bmp280.c
@@ -58,8 +58,17 @@ int bosh_bmp280_init()
 	_device.bus_read = _i2c_read;
 	_device.bus_write = _i2c_write;
 	_device.delay_msec = _delay_msec;
+	_device.dev_addr = BMP280_I2C_ADDRESS2;
 
 	int rc = bmp280_init(&_device);
+	rc += bmp280_set_filter(BMP280_FILTER_COEFF_16);
+	rc += bmp280_set_oversamp_pressure(BMP280_OVERSAMP_16X);
+	rc += bmp280_set_oversamp_temperature(BMP280_OVERSAMP_2X);
+	rc += bmp280_set_standby_durn(BMP280_STANDBY_TIME_63_MS);
+
+	rc += bmp280_set_work_mode(BMP280_ULTRA_HIGH_RESOLUTION_MODE);
+	rc += bmp280_set_power_mode(BMP280_NORMAL_MODE);
+
 	return rc;
 }
 
