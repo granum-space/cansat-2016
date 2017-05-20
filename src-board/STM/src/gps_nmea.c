4b10c54106efeb5ba31136cd1329d8a0bd80ba94
Kirs67 <kirilel200267@gmail.com>
2017-05-20 22:11:50 +0300

    Начата интеграция GPS

---
+++ src-board/STM/src/gps_nmea.c
@@ -0,0 +1,200 @@
+/* */
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "gps_nmea.h"
+
+typedef enum
+{
+	GPS_STATE_IDLE, 		// ждем доллара
+	GPS_STATE_ACCUMULATE,	// накапливаем символы
+} state_t;
+
+struct rscs_gps_t
+{
+	USART_TypeDef uart;
+	state_t state;
+	char buffer[/*FIXME RSCS_GPS_BUFFER_SIZE*/100];
+	size_t buffer_carret;
+};
+
+
+rscs_gps_t * rscs_gps_init(USART_TypeDef uartId)
+{
+	// создаем дескритор и настраиваем
+	rscs_gps_t * retval = (rscs_gps_t *)malloc(sizeof(rscs_gps_t));
+	if (NULL == retval)
+	{
+		return NULL;
+	}
+
+	retval->uart = uartId;
+	retval->buffer_carret = 0;
+	retval->state = GPS_STATE_IDLE;
+
+	// настройка UART
+	USART_InitTypeDef uartInit;
+	uartInit.USART_BaudRate = 9600;
+	uartInit.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	uartInit.USART_Mode = USART_Mode_Rx;
+	uartInit.USART_Parity = USART_Parity_No;
+	uartInit.USART_StopBits = USART_StopBits_1;
+	uartInit.USART_WordLength = USART_WordLength_8b;
+
+	USART_Init( &(retval->uart), &uartInit);
+
+	return retval;
+}
+
+
+void rscs_gps_deinit(rscs_gps_t * gps)
+{
+	USART
+	free(gps);
+}
+
+
+static int _explode(const char * str, size_t msgSize, char symbol, const char ** results,
+		size_t results_size)
+{
+	uint8_t numbersOfStr = 0;
+	for (int i = 0; i < msgSize; i++){
+		if (str[i] == symbol){
+			results[numbersOfStr] = &str[i + 1];
+			if (numbersOfStr > results_size)
+				return 0;
+			numbersOfStr++;
+		}
+	}
+	return numbersOfStr;
+}
+
+static bool _handle_message(const char * msg_signed, size_t msgSize, float * lon, float * lat,
+						    float * height, bool * hasFix)
+{
+	const uint8_t * msg = (const uint8_t *)msg_signed;
+	char chksum = msg[1]; // пропускаем нулевой символ $
+
+	size_t chksumLimit = msgSize - 5; // Пропускаем *XX\r\n
+	for (size_t i = 2; i < chksumLimit; i++)
+		chksum = chksum ^ msg[i];
+
+	int expectedChksumValue;
+	if (sscanf(msg_signed + chksumLimit+1, "%X", &expectedChksumValue) == 0)
+		return false;
+
+	if (chksum != expectedChksumValue)
+		return false;
+
+	const char * results[16];
+	uint8_t numbersOfStr = _explode(msg_signed, msgSize, ',', results, sizeof(results));
+	if (numbersOfStr == 0)
+		return false;
+
+	// разбираем долготу
+	if (sscanf(results[1], "%f", lon) != 1)
+		return false;
+
+	if(*results[2] != 'N')
+		*lon *= -1;
+
+	//разбираем широту
+	if (sscanf(results[3], "%f", lat) != 1)
+		return false;
+
+	if (*results[4] != 'E')
+		*lat *= -1;
+
+	//разбираем высоту
+	if (sscanf(results[9], "%f", height) != 1)
+		return false;
+
+	//проверка качества
+	int fixQual;
+	if (sscanf(results[5], "%i", &fixQual) != 0)
+		*hasFix = false;
+		else
+		*hasFix = true;
+	return true;
+}
+
+
+rscs_e rscs_gps_read(rscs_gps_t * gps, float * lon, float * lat,
+					 float * height, bool * hasFix)
+{
+	// нужно прочитать данные из связанного UART
+	// выкинуть все не нужное, найти нужное сообщение
+	// и выделить из него широту, долготу и высоту
+
+	char symbol;
+	size_t readed;
+
+again:
+	switch(gps->state)
+	{
+	case GPS_STATE_IDLE:
+		while(0 != (readed = rscs_uart_read_some(gps->uart, &symbol, 1)))
+		{
+			if ('$' == symbol)
+				break;
+		}
+
+		if (readed > 0 && '$' == symbol)
+		{
+			gps->state = GPS_STATE_ACCUMULATE;
+			gps->buffer[0] = symbol;
+			gps->buffer_carret = 1;
+		}
+		else
+			return RSCS_E_BUSY;
+
+		// брейк опущен сознательно
+	case GPS_STATE_ACCUMULATE:
+		while(0 != (readed = rscs_uart_read_some(gps->uart, &symbol, 1)))
+		{
+			if ('$' == symbol)
+			{
+				gps->buffer[0] = symbol;
+				gps->buffer_carret = 1;
+				continue;
+			}
+
+			if (gps->buffer_carret >= sizeof(gps->buffer))
+			{
+				gps->state = GPS_STATE_IDLE;
+				goto again;
+			}
+
+			gps->buffer[gps->buffer_carret] = symbol;
+			gps->buffer_carret += readed;
+
+			if (gps->buffer_carret == 6 && memcmp(gps->buffer, "$GPGGA", 6) != 0)
+			{
+				gps->state = GPS_STATE_IDLE;
+				goto again;
+			}
+
+			if (gps->buffer_carret >= 2
+				&& '\n' == gps->buffer[gps->buffer_carret-1]
+			    && '\r' == gps->buffer[gps->buffer_carret-2])
+			{
+				if (_handle_message(gps->buffer, gps->buffer_carret, lon, lat, height, hasFix))
+				{
+					gps->state = GPS_STATE_IDLE;
+					return RSCS_E_NONE;
+				}
+				else
+				{
+					gps->state = GPS_STATE_IDLE;
+					goto again;
+				}
+			}
+		}
+		break;
+	}
+
+	// если сообщения не нашлось, нужно вернуть RSCS_E_BUSY
+	return RSCS_E_BUSY;
+}
---
+++ src-board/STM/src/gps_nmea.h
@@ -0,0 +1,26 @@
+/* */
+
+#ifndef GPS_NMEA_H_
+#define GPS_NMEA_H_
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#include <stm32f10x_usart.h>
+
+struct rscs_gps_t;
+typedef struct rscs_gps_t rscs_gps_t;
+
+// Инициализация GPS, принимает идентификатор UART-а на котором висит GPS приёмник
+// сам открывает UART и настраивает его
+rscs_gps_t * rscs_gps_init();
+
+// Деинициализация GPS, вместе c UART, на котором он висит
+void rscs_gps_deinit(rscs_gps_t * gps);
+
+// чтение накопленных в буфере UART данных, их разбор и выделение из них широты, долготы и высоты
+// если в буфере UART еще не накопилось необходимое сообщение - вернет ошибку RSCS_E_BUSY
+int rscs_gps_read(rscs_gps_t * gps, float * lon, float * lat, float * height, bool * hasFix);
+
+#endif /* GPS_NMEA_H_ */
